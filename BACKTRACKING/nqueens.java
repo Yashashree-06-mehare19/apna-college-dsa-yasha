25ca5ff685118be38597a970a488046aafe71d0a
Yashashree <yashamehare06@gmail.com>
2025-08-16 21:57:51 +0530

    n queens , permutations

---
+++ BACKTRACKING/nqueens.java
@@ -0,0 +1,71 @@
+public class nqueens {
+
+    // Check if it's safe to place a queen at board[row][col]
+    public static boolean isSafe(char board[][], int row, int col) {
+        // vertical up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal left up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal right up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Backtracking function
+    public static void nQueens(char board[][], int row) {
+        // base case
+        if (row == board.length) {
+            printBoard(board);
+            return;
+        }
+
+        // try placing queen in each column
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q'; // place queen
+                nQueens(board, row + 1); // recursive call
+                board[row][j] = 'X'; // backtrack
+            }
+        }
+    }
+
+    // Print board
+    public static void printBoard(char board[][]) {
+        System.out.println("----- chess board -----");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 4; // change n as needed
+        char board[][] = new char[n][n];
+
+        // initialize board with 'X'
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'X';
+            }
+        }
+
+        nQueens(board, 0);
+    }
+}
---
+++ BACKTRACKING/permutations.java
@@ -0,0 +1,29 @@
+public class permutations {
+
+public static void findpermutations(String str,String ans){
+
+
+    //base case  
+
+    if(str.length()==0){
+        System.out.println(ans);
+        return ;
+    }
+
+    //recursion
+
+    for(int i=0;i<str.length();i++){
+        char curr=str.charAt(i);
+
+       String  newstr=str.substring(0, i)+str.substring(i+1);
+        findpermutations(newstr, ans+curr);
+    }
+}
+
+    public static void main(String[] args) {
+        String str="abc";
+        findpermutations(str, "");
+
+    }
+    
+}
