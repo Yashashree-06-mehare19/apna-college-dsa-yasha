bf869ceb6789084ec1050b3b278595bf24ee3a2a
Yashashree <yashamehare06@gmail.com>
2025-09-13 01:58:59 +0530

    linkedlist delete , add functions, search functions

--- LINKEDLIST/LinkedList.java
+++ LINKEDLIST/LinkedList.java
@@ -14,12 +14,13 @@ public static class Node{
 }    
 public static Node head;
 public static Node tail;
+public static int size;
 
-public void addfirst(int data){
+public static void addfirst(int data){
 
 //step 1- create new node
 Node newNode=new Node(data);
-
+size++;
 if(head==null){
         head=tail=newNode;
         return;
@@ -36,7 +37,7 @@ head=newNode;
 
 public void addLast(int data){
     Node newNode=new Node(data);
-
+    size++;
     if(head==null){ //or tail==null
         tail=head=newNode;
         return;
@@ -58,8 +59,13 @@ public void print(){
 
 
 public static void add(int idx, int data){
-    Node newNode=new Node(data);
 
+    if(idx==0){
+        addfirst(data);
+        return;
+    }
+    Node newNode=new Node(data);
+    size++;
     Node temp=head;
 
     int i=0;
@@ -72,6 +78,60 @@ public static void add(int idx, int data){
     newNode.next=temp.next;
     temp.next=newNode;
      
+}
+
+public int removefirst(){
+    if(size==0){
+        System.out.println("ll is empty");
+        return Integer.MIN_VALUE;
+    }else if(size==1){
+       int value=head.data; 
+       head=tail=null;
+       size=0;
+       return value;
+    }
+    
+    int value=head.data;
+    head=head.next;
+    size--;
+    return value;
+
+}
+
+public int removelast(){
+    if(size==0){
+        System.out.println("ll is empty ");
+    }else  if(size==1){
+        int value=head.data;
+        head=tail=null;
+        size=0;
+        return value;
+    }
+
+    //prev : i=size-2;
+    Node prev=head;
+    for(int i=0;i<size-2;i++){
+        prev=prev.next;
+    }
+   int  value=prev.next.data;//tail data
+    prev.next=null;
+    tail=prev;
+    size--;
+    return value;
+}
+
+public int itr_search(int key){
+Node temp=head;
+int i=0;
+while(temp!=null){
+    if(temp.data==key){//key found 
+        return i;
+    }
+
+    temp=temp.next;
+    i++;
+} 
+return -1;    //not found, we reached null
 }
 
 public static void main(String[] args) {
@@ -89,9 +149,16 @@ public static void main(String[] args) {
    
    ll.add(2, 90);
    ll.print();
+   
+
+   ll.removefirst();
+ll.print();
+ll.removelast();
+ll.print();
 
 
-  
+   System.out.println(ll.itr_search(3)); 
+  System.out.println(ll.itr_search(10));
     }
     
 }
